8e8048db4f1ab2e5cf7defb033ed6d5fc55b4e0e
Vicente Garate Piñeda <Vicente Garate Pieda>
2017-02-07 02:39:32 -0300

    Agregador servidor-luz

--- envia_datos/envia_datos.ino
+++ envia_datos/envia_datos.ino
@@ -4,8 +4,8 @@
 const char* ssid     = "Arial";
 const char* password = "yes09081984";
 
-byte host[] = {192,168,0,6};
-int port = 81;
+const char* host = "192.168.0.5";
+int httpPort = 81;
 
 WiFiClient client;
 
@@ -17,10 +17,8 @@ void setup() {
 
 	Serial.println();
 	Serial.println();
-	Serial.print("Tratando de conectar en ");
+	Serial.print("Conectando a ");
 	Serial.println(ssid);
-	Serial.print("Con clave: ");
-	Serial.println(password);
 
 	WiFi.begin(ssid, password);
 
@@ -30,51 +28,52 @@ void setup() {
 	}
 
 	Serial.println("");
-	Serial.println("Conectado a WiFi ");  
-	Serial.print("IP: ");
+	Serial.println("Conectado a WIFI");  
+	Serial.print("IP : ");
 	Serial.println(WiFi.localIP());
 }
 
 void loop() {
+	delay(2000);
 
-	delay(3000);
+	Serial.print("Conectando a ");
+	Serial.println(ssid);
 
-	// Creamos una instancia de WIFICLIENT 
-	Serial.print("Conectado a: ");
-	Serial.println(host);
-
-
-	if (!client.connect(host, port)) {
-		Serial.println("Sin Conexion");
+	if (!client.connect(host, httpPort)) {
+		Serial.println("No conectado...");
 		return;
 	}
 
 	// Creamos la dirección para luego usarla en el String del POST que tendremos que enviar
-	String url = "http://192.168.0.6/IOT/ingreso.php";
+	String url = "http://192.168.0.5/IOT/ingreso.php";
+
 	// creo un string con los datos que enviaré por POST lo creo de antemano para luego poder 
-	//calcular el tamaño del string dato que necesitare para enviar por post
+	// calcular el tamaño del string dato que necesitare para enviar por post
 	String data = "serie=777&temp=33";
 
 	//imprimo la url a donde enviaremos la solicitud, solo para debug
-	Serial.print("Enviando como: ");
-	Serial.println(url + data);
+	Serial.print("Enviando datos a: ");
+	Serial.println(url +"?"+ data);
 
-	  // Esta es la solicitud del tipoPOST que enviaremos al servidor
-	client.print(String("POST " + url + " HTTP/1.0\r\n" +
-		"Host: " + host + "\r\n" +
-		"Accept: *" + "/" + "*\r\n" +
-		"Content-Length: " + data.length() + "\r\n" +
-		"Content-Type: application/x-www-form-urlencoded\r\n" +
-		"\r\n" + data));
+	// Esta es la solicitud del tipoPOST que enviaremos al servidor
+	client.print(String("POST ") + url + " HTTP/1.0\r\n" +
+	"Host: " + host + "\r\n" +
+	"Accept: *" + "/" + "*\r\n" +
+	"Content-Length: " + data.length() + "\r\n" +
+	"Content-Type: application/x-www-form-urlencoded\r\n" +
+	"\r\n" + data);
 	delay(10);
 
-	// Leemos todas las lineas que nos responde el servidor y las imprimimos por pantalla, 
-	//esto no es necesario  pero es fundamental ver què me responde el servidor para localizar 
-	//fallas en la solicitud post que estoy enviando, 
-	
+	// Leemos todas las lineas que nos responde el servidor y las imprimimos por pantalla, esto no es necesario  pero es fundamental ver què me responde el servidor para localizar fallas en la solicitud post que estoy enviando, 
+	Serial.print("Respuesta del servidor:");
 	while(client.available()){
-		Serial.println("Respond:");
 		String line = client.readStringUntil('\r');
 		Serial.print(line);
 	}
+
+	Serial.println();
+
+	// se cierra la conexión
+	Serial.println("Conexion cerrada.");
+
 }
